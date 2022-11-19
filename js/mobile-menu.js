f350956dfda2b7a8714671d8025f2947bf045be3
Hanna Tubolets <anna.tubolets@gmail.com>
2022-11-19 14:46:57 +0300

    Addede mobile menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,44 +1,44 @@
-(() => {
-  const refs = {
-    openMenuBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-  }
-})();
-
-
-
-// Mobile menu with scrolls
-
 // (() => {
 //   const refs = {
 //     openMenuBtn: document.querySelector('[data-menu-open]'),
 //     closeMenuBtn: document.querySelector('[data-menu-close]'),
 //     menu: document.querySelector('[data-menu]'),
-//     body: document.querySelector('body'),
-//     menuList: document.querySelector('.mob-list'),
 //   };
 
 //   refs.openMenuBtn.addEventListener('click', toggleMenu);
 //   refs.closeMenuBtn.addEventListener('click', toggleMenu);
-//   refs.menuList.addEventListener('click', removeMenu);
 
 //   function toggleMenu() {
 //     refs.menu.classList.toggle('is-hidden');
-//     refs.body.classList.toggle('no-scroll');
-//   }
-//   function removeMenu() {
-//     refs.menu.classList.add('is-hidden');
-//     refs.body.classList.remove('no-scroll');
 //   }
 // })();
+
+
+
+// Mobile menu with scrolls
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
+    menuList: document.querySelector('.mob-list'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuList.addEventListener('click', removeMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+  function removeMenu() {
+    refs.menu.classList.add('is-hidden');
+    refs.body.classList.remove('no-scroll');
+  }
+})();
 
 
 
